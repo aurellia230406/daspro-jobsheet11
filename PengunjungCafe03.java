280e5a4c249429e050324cbc4a42292e76dd8583
ayaa <aaurellia23@gmail.com>
2024-11-19 16:09:19 +0700

    pengunjungcafe

---
+++ PengunjungCafe03.java
@@ -0,0 +1,12 @@
+public class PengunjungCafe03 {
+    
+    public static void daftarPengunjung(String... namaPengunjung) {
+        System.out.println("Daftar Nama Pengunjung: ");
+        for (int i = 0; i < namaPengunjung.length; i++) {
+            System.out.println("- " + namaPengunjung[i]);
+        }
+    }
+    public static void main(String[] args) {
+        daftarPengunjung("Agus", "Wardani", "Wilujeng", "Mardani");
+    }
+}
